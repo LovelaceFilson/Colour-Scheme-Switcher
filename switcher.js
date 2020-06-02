f6c478977b492a7504ef85af4014c253475b06ab
LovelaceFilson <58467784+LovelaceFilson@users.noreply.github.com>
2020-06-02 18:57:25 -0400

    Add files via upload

---
+++ switcher.js
@@ -0,0 +1,16 @@
+function defaultOne(color) {
+    document.body.style.background = color;  
+  }
+  
+  function defaultTwo(color) {
+    document.body.style.background = color;  
+  }
+  
+  function defaultThree(color) {
+    document.body.style.background = color;  
+  }
+  
+  function defaultFour(color) {
+    document.body.style.background = color;  
+  }
+  
